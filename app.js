b0541dd349062bfa9c7ec66b1eeb1338291be3ad
Vishal singh <63566251+vishalsingh9884@users.noreply.github.com>
2022-01-07 15:29:31 +0530

    initila commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const btn1 = document.querySelector('#btn1')
+const p1 = document.querySelector('#p1')
+btn1.addEventListener('click', () => {
+    p1.innerHTML = "Hello my name is Vishal Singh"
+})
+
+const btn2 = document.querySelector('#btn2')
+const p2 = document.querySelector('#p2')
+btn2.addEventListener('click', () => {
+    p2.innerHTML = "My Contact no. 8169886281"
+})
+
+const btn3 = document.querySelector('#btn3')
+const p3 = document.querySelector('#p3')
+btn3.addEventListener('click', () => {
+    p3.innerHTML = "I am Web/Android Developer"
+})
